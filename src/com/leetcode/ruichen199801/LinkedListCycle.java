3d53fc87983a1db6cfbd90a3888925b6d8245f32
张瑞宸 <7657599+zhangruichen@user.noreply.gitee.com>
2021-01-30 15:47:51 +0800

    Jan 30, 2021

---
+++ src/com/leetcode/ruichen199801/LinkedListCycle.java
@@ -0,0 +1,36 @@
+package com.leetcode.ruichen199801;
+
+public class LinkedListCycle {
+	
+	public class ListNode {
+		int val;
+		ListNode next;
+		
+		public ListNode(int val) {
+			this.val = val;
+		}
+	}
+
+	public class Solution {
+
+		// #141 https://leetcode-cn.com/problems/linked-list-cycle/
+		// Linked List
+		// TC = O(n), SC = O(1)
+
+		public boolean hasCycle(ListNode head) {
+			if (head == null || head.next == null) {
+				return false;
+			}
+			ListNode slow = head, fast = head;
+			while (fast != null && fast.next != null) {
+				slow = slow.next;
+				fast = fast.next.next;
+				if (slow == fast) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+}
