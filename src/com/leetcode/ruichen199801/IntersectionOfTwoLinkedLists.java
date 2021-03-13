86e74871d2ce855c1af0db8edaa77d5b21ea36a4
张瑞宸 <7657599+zhangruichen@user.noreply.gitee.com>
2021-03-13 17:48:04 +0800

    Mar 13,2021

---
+++ src/com/leetcode/ruichen199801/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,54 @@
+package com.leetcode.ruichen199801;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfTwoLinkedLists {
+	
+	// #160 https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+	// Linked List
+	
+	class ListNode {
+		int val;
+		ListNode next;
+		public ListNode(int val) {
+			this.val = val;
+		}
+	}
+	
+	public class Solution1 {
+	    // Hash Set: TC = O(m + n), SC = O(m) or O(n)
+	    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+	        Set<ListNode> set = new HashSet<>();
+	        ListNode curA = headA;
+	        while (curA != null) {
+	            set.add(curA);
+	            curA = curA.next;
+	        }
+	        ListNode curB = headB;
+	        while (curB != null) {
+	            if (set.contains(curB)) {
+	                return curB;
+	            } else {
+	                curB = curB.next;
+	            }
+	        }
+	        return null;
+	    }
+	}
+	
+	public class Solution2 {
+	    // similar to the idea of checking cycle using two pointers
+	    // if A and B do not intersect, curA == curB == null, so won't infinite loop
+	    // TC = O(m + n), SC = O(1)
+	    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+	        ListNode curA = headA, curB = headB;
+	        while (curA != curB) {
+	            curA = curA == null ? headB : curA.next;
+	            curB = curB == null ? headA : curB.next;
+	        }
+	        return curA;     
+	    }
+	}
+
+}
---
+++ src/com/leetcode/ruichen199801/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,37 @@
+package com.leetcode.ruichen199801;
+
+public class RemoveDuplicatesFromSortedList {
+	
+	class ListNode {
+		int val;
+		ListNode next;
+		public ListNode(int val) {
+			this.val = val;
+		}
+	}
+	
+	class Solution {
+
+	    // #83 https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+	    // Linked List
+	    // TC = O(n), SC = O(1)
+
+	    public ListNode deleteDuplicates(ListNode head) {
+	        if (head == null || head.next == null) {
+	            return head;
+	        }
+	        ListNode cur = head;
+	        while (cur != null && cur.next != null) {
+	            if (cur.val == cur.next.val) {
+	                cur.next = cur.next.next; 
+	                // change reference operation, skip the next node
+	            } else {
+	                cur = cur.next;
+	                // do nothing, move on to process next node
+	            }
+	        }
+	        return head;
+	    }
+	}
+
+}
