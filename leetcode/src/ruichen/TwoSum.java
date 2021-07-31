1d78c04310ce053ba24d101ab4971366110d81bd
张瑞宸 <7657599+zhangruichen@user.noreply.gitee.com>
2021-07-31 22:36:26 +0800

    Refactored for better maintainability

---
+++ leetcode/src/ruichen/TwoSum.java
@@ -0,0 +1,28 @@
+package com.leetcode.ruichen199801;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+
+            // #1 https://leetcode.com/problems/two-sum/
+            // TC = O(n), SC = O(n)
+            // Array, Hash Table
+
+            Map<Integer, Integer> numMap = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                // add array elements and check whether complement exists in hash table on the fly in one-pass
+                if (numMap.containsKey(complement)) {
+                    return new int[] {numMap.get(complement), i};
+                }
+                numMap.put(nums[i], i);
+            }
+            return null;
+        }
+    }
+
+}
